7d35204cf6b2df28d7683e0ac78d782a3361dc1c
ssp97 <zhang@yuanpai.win>
2021-08-06 09:08:34 +0800

    update

--- run.bat
+++ run.bat
@@ -2,6 +2,7 @@ go version
 go env -w GOPROXY=https://goproxy.cn,direct
 go env -w GO111MODULE=auto
 go mod tidy
-::go build -ldflags="-s -w" -o ZeroBot-App.exe
-go run main.go
+go build -ldflags="-s -w" -o ZeroBot-App.exe
+ZeroBot-App.exe
+#go run main.go
 pause
