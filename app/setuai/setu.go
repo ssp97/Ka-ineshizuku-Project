789310da223c5d1ea3ea2e7588230f0b46c9b579
ssp97 <zhang@yuanpai.win>
2022-10-14 21:15:04 +0800

    add base64 tag词接口

--- app/setuai/setu.go
+++ app/setuai/setu.go
@@ -2,6 +2,7 @@ package setuai
 
 import (
 	"encoding/base64"
+	"fmt"
 	"github.com/ssp97/Ka-ineshizuku-Project/app/publicModels"
 	"github.com/ssp97/Ka-ineshizuku-Project/pkg/zero"
 	setuaiapi "github.com/ssp97/Ka-ineshizuku-Project/pkg/setuai"
@@ -21,6 +22,26 @@ func Init(){
 
 	zero.Default().OnRegex("^!setuai (.*)$").SetBlock(true).SetPriority(20).Handle(func(ctx *ZeroBot.Ctx) {
 		tag := ctx.State["regex_matched"].([]string)[1]
+		err, url := publicModels.GetSetting(SETU_AI_URL_KEY)
+		if err!=nil{
+			ctx.SendChain(message.Text("setuai:未设置url"))
+		}
+		print("tag = ", tag)
+		ctx.SendChain(message.Text("少女祈祷中......"))
+		img,txt := setuaiapi.Request(url, &tag, nil, nil, nil, nil, nil, nil, nil)
+		imgB64 := "base64://" + base64.StdEncoding.EncodeToString(img)
+		ctx.SendChain(message.Reply(ctx.Event.MessageID), message.Image(imgB64), message.Text(txt))
+	})
+
+	zero.Default().OnRegex("^!setuai_b64 (.*)$").SetBlock(true).SetPriority(20).Handle(func(ctx *ZeroBot.Ctx) {
+		tag := ctx.State["regex_matched"].([]string)[1]
+
+		tagBytes,err := base64.StdEncoding.DecodeString(tag)
+		if err!= nil{
+			ctx.SendChain(message.Text(fmt.Sprintf("err:%s", err)))
+		}
+		tag = string(tagBytes)
+
 		err, url := publicModels.GetSetting(SETU_AI_URL_KEY)
 		if err!=nil{
 			ctx.SendChain(message.Text("setuai:未设置url"))
