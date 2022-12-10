a4a26064a48b748bbc7a9e55917a22e799b0145c
ssp97 <zhang@yuanpai.win>
2022-12-10 22:27:59 +0800

    可调超时时间和默认的文本

--- app/chatgpt/chatgpt.go
+++ app/chatgpt/chatgpt.go
@@ -9,10 +9,14 @@ import (
 	"github.com/wdvxdr1123/ZeroBot/message"
 	chatgpt_go "github.com/zhan3333/chatgpt-go"
 	log "github.com/sirupsen/logrus"
+	"strconv"
 	"time"
 )
 
 const CHATGPT_TOKEN_CODE = "chatgpt_token"
+const CHATGPT_DEFAULT_TEXT = "chatgpt_default_text"
+const CHATGPT_TIMEOUT = "chatgpt_timeout"
+
 var gC2 *chatgpt_go.ChatGPT
 var gConversationDict = make(map[int64]*chatgpt_go.Conversation)
 func Init(){
@@ -22,7 +26,7 @@ func Init(){
 	if token != ""{
 		var err error
 		log.Info("init chatgpt")
-		timeout := time.Second * 60
+		timeout := time.Second * 120
 		gC2, err = chatgpt_go.NewChatGPT(token, chatgpt_go.ChatGPTOptions{
 			Log:     logrus.NewEntry(logrus.StandardLogger()),
 			Timeout: &timeout,
@@ -33,13 +37,23 @@ func Init(){
 		}
 	}
 
+	go func() {
+		for{
+			if err := gC2.RefreshAccessToken(); err != nil {
+				log.Warn(fmt.Errorf("refresh access token: %w", err) )
+			}
+			time.Sleep(time.Minute * 1)
+		}
+
+	}()
+
 	zero.Default().OnRegex("^!chatgpt_token:(.*)$", ZeroBot.SuperUserPermission).SetPriority(-100).SetBlock(true).Handle(func(ctx *ZeroBot.Ctx) {
 		var err error
 		token := ctx.State["regex_matched"].([]string)[1]
 		publicModels.SetSetting(CHATGPT_TOKEN_CODE, token)
 
 		//gC = chatgpt.NewChatGpt(chatgpt.NewClient(chatgpt.NewCredentials(token)))
-		timeout := time.Second * 60
+		timeout := time.Second * 120
 		gC2, err = chatgpt_go.NewChatGPT(token, chatgpt_go.ChatGPTOptions{
 			Log:     logrus.NewEntry(logrus.StandardLogger()),
 			Timeout: &timeout,
@@ -51,7 +65,28 @@ func Init(){
 		}else{
 			ctx.SendChain(message.Text("设置成功"))
 		}
+	})
 
+	zero.Default().OnRegex("^!chatgpt_timeout:(.*)$", ZeroBot.SuperUserPermission).SetPriority(-100).SetBlock(true).Handle(func(ctx *ZeroBot.Ctx) {
+		string := ctx.State["regex_matched"].([]string)[1]
+		second, err := strconv.Atoi(string)
+		if err != nil{
+			ctx.SendChain(message.Text("设置失败"))
+			return
+		}
+		//publicModels.SetSetting(CHATGPT_TIMEOUT, token)
+		gC2.Timeout = time.Second * time.Duration(second)
+
+		ctx.SendChain(message.Text(fmt.Sprintf("设置成功，现在超时时间为%d秒", second)))
+
+	})
+
+	zero.Default().OnRegex("^!chatgpt_default_text:((?:.|\n)*?)$", ZeroBot.SuperUserPermission).SetPriority(-100).SetBlock(true).Handle(func(ctx *ZeroBot.Ctx) {
+		token := ctx.State["regex_matched"].([]string)[1]
+		publicModels.SetSetting(CHATGPT_DEFAULT_TEXT, token)
+
+
+		ctx.SendChain(message.Text("设置成功"))
 
 	})
 
@@ -84,6 +119,20 @@ func Init(){
 		}
 
 		gConversationDict[uid] = gC2.NewConversation("", "")
-		ctx.SendChain(message.Text("我好像...失忆了"))
+
+		err, defaultText  := publicModels.GetSetting(CHATGPT_DEFAULT_TEXT)
+		if defaultText != "" && err != nil{
+			resp, err := gConversationDict[uid].SendMessage(defaultText)
+			if err != nil {
+				// Handle err
+				ctx.SendChain(message.Text("我好像...失忆了"))
+				return
+			}
+			ctx.SendChain(message.Reply(ctx.Event.MessageID), message.Text(resp))
+		}else{
+			ctx.SendChain(message.Text("我好像...失忆了"))
+		}
+
+
 	})
 }
